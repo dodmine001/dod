23de91eb9b04919ea116449a532da0015be2e78b
dodmine001 <dodmine001@outlook.com>
2024-10-26 07:35:11 +0530

    Update m1.sh

--- m1.sh
+++ m1.sh
@@ -3,3 +3,4 @@ source $HOME/.cargo/env
 git init .
 git pull https://github.com/DOD-Blockchain/miner-rust
 cargo build --release
+./target/release/dod_miner miner --cycles_price=5 --wif=KxJbXSLEQPH2uqioe28seaKURnPEJqnmKgm6uhr645iC2XwypKmg
